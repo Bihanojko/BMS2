005834a5fa7fb1cadfb6affe246259249d1ef613
Nikola Valesova <bihanojko@Nikola-MacBook-Pro.local>
2018-11-21 17:54:17 +0100

    Created basic structure, added libraries

--- bms2A.cpp
+++ bms2A.cpp
@@ -2,7 +2,7 @@
  * Project 2
  * Subject: Wireless and Mobile Networks
  * Author:  Nikola Valesova
- * Date:    TODO
+ * Date:    16. 12. 2018
  * Faculty of Information Technology
  * Brno University of Technology
  * File:    bms2A.cpp
@@ -12,38 +12,116 @@
 // #include <math.h>
 #include <iostream>
 #include <fstream>
+#include <cmath>
 // #include <iomanip>
+
+#include "lib/ecc.h"
+
 
 // create and open output file
 std::ofstream CreateOutputFile(std::string filename)
 {
     std::ofstream outputFile;
-    outputFile.open((filename + ".out").c_str());
+    outputFile.open(filename.append(".out"), std::ofstream::binary);
 
     return outputFile;
+}
+
+
+// determine file length
+int GetFileLength(std::ifstream& inputFile)
+{
+    inputFile.seekg(0, inputFile.end);
+    int length = inputFile.tellg();
+    inputFile.seekg(0, inputFile.beg);
+
+    return length;
+}
+
+
+// TODO
+void encodeInput()
+{
+	unsigned mapIndex;
+	size_t encodedLength = 0;
+	size_t dataLength = dataSize;
+
+	size_t encodedSize = (size / dataSize) * blockSize;
+	size_t lastBlockSize = size % dataSize + paritySize;
+	if (size % dataSize != 0)
+		encodedSize += lastBlockSize;
+
+	size_t blocks = std::ceil(encodedSize / (double)blockSize);
+	encoded.clear();
+	encoded.resize(encodedSize);
+	unsigned blocksCounter = 0;
+
+	while(encodedLength < size)
+	{
+		// For last block
+		if(dataLength + encodedLength > size)
+			dataLength = size - encodedLength;
+		encode_data(input + encodedLength, dataLength, output);
+		// Save encoded data to vector + interleave it
+		for(int i=0; i<dataLength + paritySize; ++i)
+		{
+			mapIndex = blocksCounter + i * blocks;
+			// Need to adjust index if last codeword is smaller
+			if(i > lastBlockSize)
+				mapIndex -= i - lastBlockSize;;
+			encoded[mapIndex] = output[i];
+		}
+		encodedLength += dataSize;
+		// Move to next codeword
+		++blocksCounter;
+		// Without interleaving
+		//encoded.insert(encoded.end(), output, output + dataLength + paritySize);
+	}
 }
 
 
 // MAIN
 int main(int argc, char** argv)
 {
+    // check argument count
     if (argc != 2)
     {
         std::cerr <<  "Invalid argument count! Usage: ./bms2A filename" << std::endl;
         return EXIT_FAILURE;
     }
 
+    // open input file
     std::string filename = argv[1];
     std::ifstream inputFile;
-    inputFile.open(filename);
+    inputFile.open(filename, std::ifstream::binary);
 
+    // check if input file was opened successfully
     if (!inputFile.is_open())
     {
         std::cerr <<  "Unable to open input file: " << filename << std::endl;
         return EXIT_FAILURE;
     }
 
+    // determine input file length
+    int inputFileLength = GetFileLength(inputFile);
+
+    // allocate memory
+    unsigned char* buffer = new unsigned char [inputFileLength];
+
+    // read data as a block
+    inputFile.read(reinterpret_cast<char*>(buffer), inputFileLength);
+
+    // initialize error checking and correcting
+    initialize_ecc();
+
+	// TODO 
+    encodeInput();
+
+    // create and open output file
     std::ofstream outputFile = CreateOutputFile(filename);
+
+	// TODO 
+    outputFile.write((const char *)&encoded.front(), encoded.size());
 
     outputFile.close();
     inputFile.close();
--- bms2B.cpp
+++ bms2B.cpp
@@ -2,7 +2,7 @@
  * Project 2
  * Subject: Wireless and Mobile Networks
  * Author:  Nikola Valesova
- * Date:    TODO
+ * Date:    16. 12. 2018
  * Faculty of Information Technology
  * Brno University of Technology
  * File:    bms2B.cpp
@@ -12,38 +12,123 @@
 // #include <math.h>
 #include <iostream>
 #include <fstream>
+#include <cmath>
+
+#include "lib/ecc.h"
 
 
 // create and open output file
 std::ofstream CreateOutputFile(std::string filename)
 {
     std::ofstream outputFile;
-    outputFile.open((filename + ".ok").c_str());
+    outputFile.open(filename.append(".ok"), std::ofstream::binary);
 
     return outputFile;
+}
+
+
+// determine file length
+int GetFileLength(std::ifstream& inputFile)
+{
+    inputFile.seekg(0, inputFile.end);
+    int length = inputFile.tellg();
+    inputFile.seekg(0, inputFile.beg);
+
+    return length;
+}
+
+
+// TODO
+void deInterleave()
+{
+    std::vector<unsigned char> deInterleaved;
+	deInterleaved.resize(inputFileLength);
+
+	size_t blocks = std::ceil(size/(double)blockSize);
+	size_t lastBlockSize = size % blockSize;
+	size_t dataLength = dataSize;
+	for(int j=0; j< blocks; j++) {
+		if(j == blocks - 1) {
+			dataLength = lastBlockSize - paritySize;
+		}
+		for(int i=0; i<dataLength + paritySize; ++i)
+		{
+			int map = j + i * blocks;
+			if(i > lastBlockSize)
+				map -= i - lastBlockSize;
+			deInterleaved[i + j * blockSize] = input[map];
+		}
+	}
+}
+
+
+// TODO
+void decodeInput(std::ofstream& outputFile)
+{
+	deInterleave();
+	size_t decoded = 0;
+	size_t blockLength = blockSize;
+	size_t writeSize = dataSize;
+
+	while(decoded < size)
+	{
+		// If it is last block, adjust size
+		if (blockLength + decoded >= size)
+		{
+			blockLength = size - decoded;
+			writeSize = blockLength - paritySize;
+		}
+		// Decode date
+		decode_data(deInterleaved.data() + decoded, blockLength);
+		// Corrector errors
+		if (check_syndrome() != 0)
+			correct_errors_erasures(deInterleaved.data() + decoded, blockLength, 0, nullptr);
+
+		outputFile.write(reinterpret_cast<char*>(deInterleaved.data() + decoded), writeSize);
+		decoded += blockLength;
+	}
 }
 
 
 // MAIN
 int main(int argc, char** argv)
 {    
+    // check argument count
     if (argc != 2)
     {
 		std::cerr <<  "Invalid argument count! Usage: ./bms2B filename" << std::endl;
 		return EXIT_FAILURE;
     }
 
+    // open input file
     std::string filename = argv[1];
     std::ifstream inputFile;
-    inputFile.open(filename);
+    inputFile.open(filename, std::ifstream::binary);
 
+    // check if input file was opened successfully
     if (!inputFile.is_open())
     {
         std::cerr <<  "Unable to open input file: " << filename << std::endl;
         return EXIT_FAILURE;
     }
 
+    // determine input file length
+    int inputFileLength = GetFileLength(inputFile);
+
+    // allocate memory
+    unsigned char* buffer = new unsigned char [inputFileLength];
+
+    // read data as a block
+    inputFile.read(reinterpret_cast<char*>(buffer), inputFileLength);
+
+    // create and open output file
     std::ofstream outputFile = CreateOutputFile(filename);
+
+    // initialize error checking and correcting
+	initialize_ecc();
+
+    // TODO
+	decodeInput(outputFile);
 
     outputFile.close();
     inputFile.close();
